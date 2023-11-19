ec5461be5531890cc77eed70f3006456430e4bd2
AlexanderDerevliov <alex.feelred@gmail.com>
2023-11-19 12:50:07 +0100

    almost ended exercise

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
-const { createApp } = Vue;
 
-const app = createApp({
-    data() {
-        return {
+const {createApp} = Vue 
+
+createApp({
+    data(){
+        return{
             contacts: [
                 {
-                    name: 'Michele',
-                    avatar: '_1',
+                    name: 'Fabio Foresti',
+                    avatar: ' ./img/avatar_1.jpg',
                     visible: true,
-                    messages: [
+                    messages: [ 
                         {
                             date: '10/01/2020 15:30:55',
-                            message: 'Hai portato a spasso il cane?',
+                            message: 'Hai fatto la valutazione?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            message: 'Ricordati di stendere i panni',
+                            message: 'Come è andata?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 16:15:22',
-                            message: 'Tutto fatto!',
+                            message: 'Tutto fatto! Questo mese si fattura',
                             status: 'received'
                         }
                     ],
                 },
                 {
-                    name: 'Fabio',
-                    avatar: '_2',
+                    name: 'Alessio',
+                    avatar: './img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -38,41 +39,41 @@ const app = createApp({
                         },
                         {
                             date: '20/03/2020 16:30:55',
-                            message: 'Bene grazie! Stasera ci vediamo?',
+                            message: 'Bene grazie! Sasera lavoro e tu?',
                             status: 'received'
                         },
                         {
                             date: '20/03/2020 16:35:00',
-                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            message: 'Io sono libero, buon lavoro allora',
                             status: 'sent'
                         }
                     ],
                 },
                 {
-                    name: 'Samuele',
-                    avatar: '_3',
+                    name: 'Marco lavoro',
+                    avatar: './img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
                             date: '28/03/2020 10:10:40',
-                            message: 'La Marianna va in campagna',
+                            message: 'Domani a che ora?',
                             status: 'received'
                         },
                         {
                             date: '28/03/2020 10:20:10',
-                            message: 'Sicuro di non aver sbagliato chat?',
+                            message: 'Solita ora, a domani',
                             status: 'sent'
                         },
                         {
                             date: '28/03/2020 16:15:22',
-                            message: 'Ah scusa!',
+                            message: 'A domani!',
                             status: 'received'
                         }
                     ],
                 },
                 {
-                    name: 'Alessandro B.',
-                    avatar: '_4',
+                    name: 'Kevin',
+                    avatar: './img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -88,8 +89,8 @@ const app = createApp({
                     ],
                 },
                 {
-                    name: 'Alessandro L.',
-                    avatar: '_5',
+                    name: 'Zio Giovanni',
+                    avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -105,35 +106,40 @@ const app = createApp({
                     ],
                 },
                 {
-                    name: 'Claudia',
-                    avatar: '_6',
+                    name: 'Ilaria ',
+                    avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
-                            message: 'Ciao Claudia, hai novità?',
+                            message: 'Ciao amore, come stai?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            message: 'Non ancora',
+                            message: 'Bene amore e tu?',
                             status: 'received'
                         },
                         {
                             date: '10/01/2020 15:51:00',
-                            message: 'Nessuna nuova, buona nuova',
+                            message: 'Anche io ma mi manchi molto',
                             status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:53:00',
+                            message: 'Anche tu mi manchi',
+                            status: 'received'
                         }
                     ],
                 },
                 {
-                    name: 'Federico',
-                    avatar: '_7',
+                    name: 'Simone',
+                    avatar: './img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
-                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            message: 'Fai gli auguri a Zia che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
@@ -145,7 +151,7 @@ const app = createApp({
                 },
                 {
                     name: 'Davide',
-                    avatar: '_8',
+                    avatar: './img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
@@ -165,11 +171,40 @@ const app = createApp({
                         }
                     ],
                 }
-            ]
+            ],
+            activeContact: 0,
+            newMessage: '',
+            reply: {message: 'Ciaoo!!',
+                    status: 'received'
+                   },
+            search: ''
+        }
+    },
+    methods: {
+        addNewMessage(newMessage){
+            if(newMessage === ''){
+
+                return false
+
+            }
+            this.newMessage = {message: newMessage,
+                               status: 'sent'}
+
+            this.contacts[this.activeContact].messages.push(this.newMessage)
+            
+            setTimeout(this.giveReply, 1500)
+            
+            this.newMessage = ''     
+        },
+        giveReply(){
+            this.contacts[this.activeContact].messages.push(this.reply)
+        }
+    },
+    computed: {
+        filteredResult(){
+           return this.contacts.filter(contact =>{
+           return contact.name.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
+           }) 
         }
     }
-
-
-});
-
-app.mount('#boolzapp');
+}).mount('#app')
